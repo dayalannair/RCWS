5d890d0648ddf7147207110d16a3b748d8031508
dayalannair <nairdayalan@gmail.com>
2022-06-30 20:52:11 +0200

    G Window goood

--- current_state/windowing_rd.m
+++ current_state/windowing_rd.m
@@ -7,12 +7,28 @@ n_samples = size(iq_u,2);
 n_sweeps = size(iq_u,1);
 
 %%
-gwin = gausswin(n_samples);
+nfft = 256;
+fs = 200e3;
+f = f_ax(nfft, fs);
+f = f/1000;
 
 u = iq_u(200,:).';
-
+gwin = gausswin(n_samples);
 uwin = u.*gwin;
+UWIN = fft(uwin, nfft);
+U = fft(u, nfft);
 
 close all
 figure
-plot(real(uwin))
+plot(f, 10*log10(fftshift(abs(UWIN))), 'DisplayName','Gaussian Window')
+title("Effect of windowing on signal spectrum for 1024 point FFT")
+ylabel("Magnitude (dB)")
+xlabel("Freqeuncy (kHz)")
+hold on
+plot(f, 10*log10(fftshift(abs(U))),'DisplayName','No Window')
+legend
+
+% NOTES: Does not seem to improve much after 1024 point fft
+
+
+% plot(real(uwin))
