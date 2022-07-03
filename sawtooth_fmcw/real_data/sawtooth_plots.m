c3ec70de4002fd0b7e17e8d93b239a3840eb8473
dayalannair <nairdayalan@gmail.com>
2022-07-03 12:42:13 +0200

    wip

--- sawtooth_fmcw/real_data/sawtooth_plots.m
+++ sawtooth_fmcw/real_data/sawtooth_plots.m
@@ -6,20 +6,27 @@ q_dat = table2array(iq_tbl(subset,201:400));
 iq = i_dat + 1i*q_dat;
 %%
 
+n_samples = size(i_dat,2);
+n_sweeps = size(i_dat,1);
 time = iq_tbl.Var401;
 t0 = time(1);
 t_sweeps = time - t0;
 periods = diff(t_sweeps);
 avg_period = mean(periods);
+
+t = zeros(n_sweeps, n_samples);
+for i = length(t)
+    t(i,:) = linspace(t_sweeps(i),t_sweeps(i+1), n_samples);
+end
 %%
 close all
 figure
 tiledlayout(3,1)
 nexttile
-plot(i_dat')
+plot(t',i_dat')
 title("Sawtooth I data")
 nexttile
-plot(q_dat')
+plot(t', q_dat')
 title("Sawtooth Q data")
 nexttile
 plot(real(iq.'))
