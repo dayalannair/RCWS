8c85759b80a03a954fbb781d7d4d76af1bb34858
dayalannair <nairdayalan@gmail.com>
2022-07-09 17:01:27 +0200

    CW fft

--- dual_cw_fmcw/cw_dsp.m
+++ dual_cw_fmcw/cw_dsp.m
@@ -2,7 +2,7 @@ addpath('../../library/');
 addpath('../urad_control/urad_pi/')
 % Import data
 %iq_tbl=readtable('IQ_sawtooth4096_backyrd.txt', 'Delimiter' ,' ');
-iq_tbl=readtable('iq_CW_fmcw_15-39-24.txt', 'Delimiter' ,' ');
+iq_tbl=readtable('iq_CW_fmcw_15-58-25.txt', 'Delimiter' ,' ');
 i_dat = table2array(iq_tbl(:,1:200));
 q_dat = table2array(iq_tbl(:,201:400));
 iq = i_dat + 1i*q_dat;
@@ -14,5 +14,13 @@ n_sweeps = size(i_dat,1);
 IQ = fft(iq,[],2);
 close all
 figure
+
+for sweep = 1:n_sweeps
+    plot(sftmagdb(IQ(sweep,:)).')
+    pause(0.05)
+end
+
+
+
 plot(sftmagdb(IQ).')
-plot(fftshift(angle(IQ.')))
+% plot(fftshift(angle(IQ.')))
