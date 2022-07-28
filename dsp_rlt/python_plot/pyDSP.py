5b512484b4f08c2e22641d45caebff546d17fa10
dayalannair <nairdayalan@gmail.com>
2022-07-28 16:09:54 +0200

    working in real time

---
+++ dsp_rlt/python_plot/pyDSP.py
@@ -0,0 +1,68 @@
+# from cfar_lib import os_cfar
+from operator import length_hint
+from turtle import up
+from os_cfar_v2 import os_cfar
+import numpy as np
+from scipy.fft import fft
+from scipy import signal
+
+def py_trig_dsp(i_data, q_data):
+
+    # SQUARE LAW DETECTOR
+    # NOTE: last element in slice not included
+    iq_u = np.power(i_data[  0:200],2) + np.power(q_data[  0:200],2)
+    iq_d = np.power(i_data[200:400],2) + np.power(q_data[200:400],2)
+
+    # TAYLOR WINDOW
+    # SLL specified as positive
+    twinu = signal.windows.taylor(200, nbar=4, sll=38, norm=False)
+    twind = signal.windows.taylor(200, nbar=4, sll=38, norm=False)
+    iq_u = np.multiply(iq_u, twinu)
+    iq_d = np.multiply(iq_u, twind)
+
+    # 512-point FFT
+    n_fft = 512 
+    IQ_UP = fft(iq_u,n_fft)
+    IQ_DN = fft(iq_d,n_fft)
+
+    # Halve FFTs
+    # note: python starts from zero for this!
+    IQ_UP = IQ_UP[0:round(n_fft/2)]
+    IQ_DN = IQ_DN[round(n_fft/2):]
+    # print(len(IQ_UP))   
+    # Null feedthrough
+    nul_width_factor = 0.04
+    num_nul = round((n_fft/2)*nul_width_factor)
+    # note: python starts from zero for this!
+    IQ_UP[0:num_nul-1] = 0
+    IQ_DN[len(IQ_DN)-num_nul:] = 0
+    # print(len(IQ_UP))
+    IQ_DN = np.flip(IQ_DN)
+    # OS CFAR
+    n_samples = len(iq_u)
+    half_guard = n_fft/n_samples
+    half_guard = int(np.floor(half_guard/2)*2) # make even
+
+    half_train = round(20*n_fft/n_samples)
+    half_train = int(np.floor(half_train/2))
+    rank = 2*half_train -2*half_guard
+    # rank = half_train*2
+    Pfa_expected = 15e-3
+    # factorial needs integer values
+    SOS = 2
+    # note the abs
+
+    Pfa, cfar_res_up, upth = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_UP))
+    Pfa, cfar_res_dn, dnth = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_DN))
+
+    np.log(upth, out=upth)
+    np.log(dnth, out=dnth)
+    np.log(abs(IQ_UP), out=IQ_UP)
+    np.log(abs(IQ_DN), out=IQ_DN)
+
+
+
+    # log scale for display purposes
+    return cfar_res_up, cfar_res_dn, upth, dnth, IQ_UP, IQ_DN
+    # return cfar_res_up, cfar_res_dn, 20*np.log10(upth), 20*np.log10(dnth),\
+    #      20*np.log10(abs(IQ_UP), 10),  20*np.log10(abs(IQ_DN))
---
+++ dsp_rlt/python_plot/pyPlotDSP.py
@@ -0,0 +1,168 @@
+import uRAD_USB_SDK11
+import serial
+from time import time, sleep, strftime,localtime
+import sys
+from pyDSP import py_trig_dsp
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+# True if USB, False if UART
+usb_communication = True
+
+try:
+	mode_in = str(sys.argv[1])
+	BW = int(sys.argv[2])
+	Ns = int(sys.argv[3])
+	sweeps = int(sys.argv[4])
+	t = localtime()
+	now = strftime("%H-%M-%S", t)  
+	fs = 200000
+	runtime = sweeps*Ns/200000
+	if mode_in == "s":
+		print("********** SAWTOOTH MODE **********")
+		resultsFileName = 'IQ_saw_' + str(BW) + '_' + str(Ns) +  '_' + str(now) + '.txt'
+		mode = 2					
+	elif mode_in == "t":
+		print("********** TRIANGLE MODE **********")
+		resultsFileName = 'IQ_tri_' + str(BW) + '_' + str(Ns) + '_' + str(now) + '.txt'
+		mode = 3	
+	elif mode_in == "d":
+		print("********** DUAL RATE MODE **********")
+		resultsFileName = 'IQ_dual_' + str(BW) + '_' + str(Ns) + '_' + str(now) + '.txt'
+		mode = 4					
+	else: 
+		print("Invalid mode")
+		exit()
+	print("BW = ",str(BW),"\nNs = ",str(Ns),"\nSweeps = ",str(sweeps))
+	print("Expected run time (saw): ",str(runtime))
+except: 
+	print("Invalid mode")
+	exit()
+
+# input parameters
+# BW and Ns input as arguments
+f0 = 5						# starting at 24.005 GHz
+I_true = True 				# In-Phase Component (RAW data) requested
+Q_true = True 				# Quadrature Component (RAW data) requested
+
+# Serial Port configuration
+ser = serial.Serial()
+if (usb_communication):
+	ser.port = 'COM3'
+	# ser.port = '/dev/ttyACM0'
+	ser.baudrate = 1e6
+else:
+	print("Could not find USB connection.")
+	exit()
+	# ser.port = '/dev/serial0'
+	# ser.baudrate = 115200
+
+# Sleep Time (seconds) between iterations
+timeSleep = 5e-3
+
+# Other serial parameters
+ser.bytesize = serial.EIGHTBITS
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.timeout = 1
+
+# Method to correctly turn OFF and close uRAD
+def closeProgram():
+	# switch OFF uRAD
+	return_code = uRAD_USB_SDK11.turnOFF(ser)
+	if (return_code != 0):
+		print("ERROR: Ending")
+		exit()
+
+# Open serial port
+try:
+	ser.open()
+except:
+	print("COM port failed to open")
+	closeProgram()
+
+# switch ON uRAD
+return_code = uRAD_USB_SDK11.turnON(ser)
+if (return_code != 0):
+	print("uRAD failed to turn on")
+	closeProgram()
+
+if (not usb_communication):
+	sleep(timeSleep)
+
+# loadConfiguration uRAD
+return_code = uRAD_USB_SDK11.loadConfiguration(ser, mode, f0, BW, Ns, 0, 0, 0, 0, 0, 0, 0, 0, I_true, Q_true, 0)
+if (return_code != 0):
+	print("uRAD configuration failed")
+	closeProgram()
+
+if (not usb_communication):
+	sleep(timeSleep)
+
+t_0 = time()
+i = 0
+I = []
+Q = []
+return_code, results, raw_results = uRAD_USB_SDK11.detection(ser)
+if (return_code != 0):
+	closeProgram()
+		# I.append(raw_results[0])
+		# Q.append(raw_results[1])
+		# call matlab dsp
+I = raw_results[0]
+Q = raw_results[1]
+cfar_res_up, cfar_res_dn, upth, dnth, fftu, fftd = py_trig_dsp(I,Q)
+plt.ion()
+# x = np.zeros(100, 100)
+# y = np.zeros(100, 100)
+figure, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 8))
+line1, = ax[0].plot(fftu)
+line2, = ax[0].plot(upth)
+line3, = ax[1].plot(fftd)
+line4, = ax[1].plot(dnth)
+# eng.eval("load(\'urad_trig_proc_config.mat\')")
+print("System running...")
+try:
+	for i in range(sweeps):
+		return_code, results, raw_results = uRAD_USB_SDK11.detection(ser)
+		if (return_code != 0):
+			closeProgram()
+		# I.append(raw_results[0])
+		# Q.append(raw_results[1])
+		# call matlab dsp
+		I = raw_results[0]
+		Q = raw_results[1]
+		t0_proc = time()
+		cfar_res_up, cfar_res_dn, upth, dnth, fftu, fftd = py_trig_dsp(I,Q)
+		t1_proc = time()-t0_proc
+		line1.set_ydata(fftu)
+		line2.set_ydata(upth)
+		line3.set_ydata(fftd)
+		line4.set_ydata(dnth)
+		figure.canvas.draw()
+
+		figure.canvas.flush_events()
+		# plt.plot(fftu)
+		# plt.show()
+		# sleep(1)
+		# plot1.set_xdata(x)
+		# plot1.set_ydata(update_y_value)
+	
+		# figure.canvas.draw()
+		# figure.canvas.flush_events()
+
+		# print("Processing time: ", t1_proc)
+		# if eng.workspace['safety']<10:
+		# 	print("Range of hazardous target: ", eng.workspace['targ_rng'])
+		# 	print("Speed of hazardous target: ", eng.workspace['targ_vel'])
+		# 	print("TOA of hazardous target: ", eng.workspace['safety'])
+		
+
+	print("Elapsed time: ", str(time()-t_0))
+
+	print("Complete.")
+	
+except KeyboardInterrupt:
+	uRAD_USB_SDK11.turnOFF(ser)
+	print("Interrupted.")
+	exit()
