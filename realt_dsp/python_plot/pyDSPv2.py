fabbf7693b2a43cba6305dcfbf99028a80109e20
dayalannair <nairdayalan@gmail.com>
2022-08-08 12:30:24 +0200

    little wp

--- realt_dsp/python_plot/pyDSPv2.py
+++ realt_dsp/python_plot/pyDSPv2.py
@@ -8,124 +8,145 @@ from scipy import signal
 
 def py_trig_dsp(i_data, q_data):
 
-    # SQUARE LAW DETECTOR
-    # NOTE: last element in slice not included
-    iq_u = np.power(i_data[  0:200],2) + np.power(q_data[  0:200],2)
-    iq_d = np.power(i_data[200:400],2) + np.power(q_data[200:400],2)
+	# SQUARE LAW DETECTOR
+	# NOTE: last element in slice not included
+	iq_u = np.power(i_data[  0:200],2) + np.power(q_data[  0:200],2)
+	iq_d = np.power(i_data[200:400],2) + np.power(q_data[200:400],2)
 
-    # TAYLOR WINDOW
-    # SLL specified as positive
-    twin = signal.windows.taylor(200, nbar=3, sll=100, norm=False)
-    iq_u = np.multiply(iq_u, twin)
-    iq_d = np.multiply(iq_d, twin)
+	# TAYLOR WINDOW
+	# SLL specified as positive
+	twin = signal.windows.taylor(200, nbar=3, sll=100, norm=False)
+	iq_u = np.multiply(iq_u, twin)
+	iq_d = np.multiply(iq_d, twin)
 
-    # 512-point FFT
-    n_fft = 512 
-    IQ_UP = fft(iq_u,n_fft)
-    IQ_DN = fft(iq_d,n_fft)
+	# 512-point FFT
+	n_fft = 512 
+	IQ_UP = fft(iq_u,n_fft)
+	IQ_DN = fft(iq_d,n_fft)
 
-    # Halve FFTs
-    # note: python starts from zero for this!
-    IQ_UP = IQ_UP[0:round(n_fft/2)]
-    IQ_DN = IQ_DN[round(n_fft/2):]
+	# Halve FFTs
+	# note: python starts from zero for this!
+	IQ_UP = IQ_UP[0:round(n_fft/2)]
+	IQ_DN = IQ_DN[round(n_fft/2):]
 
-    # print(len(IQ_UP))   
-    # Null feedthrough
-    nul_width_factor = 0.04
-    num_nul = round((n_fft/2)*nul_width_factor)
-    # note: python starts from zero for this!
-    IQ_UP[0:num_nul-1] = 0
-    IQ_DN[len(IQ_DN)-num_nul:] = 0
-    # print(len(IQ_UP))
-    IQ_DN = np.flip(IQ_DN)
-    # OS CFAR
-    n_samples = len(iq_u)
-    half_guard = n_fft/n_samples
-    half_guard = int(np.floor(half_guard/2)*2) # make even
+	# print(len(IQ_UP))   
+	# Null feedthrough
+	nul_width_factor = 0.04
+	num_nul = round((n_fft/2)*nul_width_factor)
+	# note: python starts from zero for this!
+	IQ_UP[0:num_nul-1] = 0
+	IQ_DN[len(IQ_DN)-num_nul:] = 0
+	# print(len(IQ_UP))
+	IQ_DN = np.flip(IQ_DN)
+	# OS CFAR
+	n_samples = len(iq_u)
+	half_guard = n_fft/n_samples
+	half_guard = int(np.floor(half_guard/2)*2) # make even
 
-    half_train = round(20*n_fft/n_samples)
-    half_train = int(np.floor(half_train/2))
-    rank = 2*half_train -2*half_guard
-    # rank = half_train*2
-    Pfa_expected = 15e-3
-    # factorial needs integer values
-    SOS = 2
-    # note the abs
+	half_train = round(20*n_fft/n_samples)
+	half_train = int(np.floor(half_train/2))
+	rank = 2*half_train -2*half_guard
+	# rank = half_train*2
+	Pfa_expected = 15e-3
+	# factorial needs integer values
+	SOS = 2
+	# note the abs
 
-    # -------------------- CFAR detection ---------------------------
-    Pfa, cfar_res_up, upth = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_UP))
-    Pfa, cfar_res_dn, dnth = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_DN))
+	# -------------------- CFAR detection ---------------------------
+	Pfa, cfar_res_up, upth = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_UP))
+	Pfa, cfar_res_dn, dnth = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_DN))
 
-    # np.log(upth, out=upth)
-    # np.log(dnth, out=dnth)
-    # np.log(abs(IQ_UP), out=IQ_UP)
-    # np.log(abs(IQ_DN), out=IQ_DN)
+	# np.log(upth, out=upth)
+	# np.log(dnth, out=dnth)
+	# np.log(abs(IQ_UP), out=IQ_UP)
+	# np.log(abs(IQ_DN), out=IQ_DN)
 
-    upth = 20*np.log(upth)
-    dnth = 20*np.log(dnth)
-    IQ_UP = 20*np.log(abs(IQ_UP))
-    IQ_DN = 20*np.log(abs(IQ_DN))
-    cfar_res_up = 20*np.log(abs(cfar_res_up))
-    cfar_res_dn = 20*np.log(abs(cfar_res_dn))
+	upth = 20*np.log(upth)
+	dnth = 20*np.log(dnth)
+	IQ_UP = 20*np.log(abs(IQ_UP))
+	IQ_DN = 20*np.log(abs(IQ_DN))
+	os_pku = 20*np.log(abs(cfar_res_up))
+	os_pkd = 20*np.log(abs(cfar_res_dn))
 
-    nbins = 16
-    bin_width = (n_fft/2)/nbins
-    fbu = np.zeros(n_sweeps,nbins)
-    fbd = np.zeros(n_sweeps,nbins)
+	nbins = 16
+	bin_width = (n_fft/2)/nbins
+	fbu = np.zeros(nbins)
+	fbd = np.zeros(nbins)
 
-    rg_array = np.zeros(n_sweeps,nbins)
-    fd_array = np.zeros(n_sweeps,nbins)
-    sp_array = np.zeros(n_sweeps,nbins)
-    sp_array_corrected = np.zeros(n_sweeps,nbins)
-    beat_arr = np.zeros(n_sweeps,nbins)
+	rg_array = np.zeros(nbins)
+	fd_array = np.zeros(nbins)
+	sp_array = np.zeros(nbins)
+	sp_array_corrected = np.zeros(nbins)
+	beat_arr = np.zeros(nbins)
+	fs = 200e3
+	lmda = 0.0125
+	# tsweep = 1e-3
+	# bw = 240e6
+	# # can optimise out this calculation
+	# slope = bw/tsweep
+	slope = 2.4e11
+	c = 3e8
+	f_ax = np.linspace(0, round(fs/2), round(n_fft/2))
+	road_width = 2
+	correction_factor = 3
+	fd_max = 2.6667e3 # for max speed = 60km/h
+	# ********************* beat extraction for multiple targets **************************
+	for bin in range(nbins):
+		# find beat in bin
+		bin_slice_d = os_pkd[bin*bin_width:(bin+1)*bin_width]
+		[magd, idx_d] = max(bin_slice_d)
+		
+		beat_index = bin*bin_width + idx_d
+		if magd != 0:
+			fbd[bin] = f_ax(beat_index)
+			# set up bin slice to range of expected beats
+			# See freqs from 0 to index 15 - determined from 60kmh (VERIFY)
+			# check if far enough from center
+			if (beat_index>15):
+				bin_slice_u = os_pku[beat_index - 15:beat_index]
+			# if not, start from center
+			else:
+				bin_slice_u = os_pku[1:beat_index]
+				
+			# index is index in the subset
+			[magu, idx_u] = max(bin_slice_u)
+			if magu != 0:
+				fbu[bin] = f_ax[beat_index - 15 + idx_u]
 
-    # ------------- beat extraction for multiple targets ----------
-    for bin = 0:(nbins-1):
-        # find beat in bin
-        bin_slice_d = os_pkd(i,bin*bin_width+1:(bin+1)*bin_width)
-        [magd, idx_d] = max(bin_slice_d)
-        
-        beat_index = bin*bin_width + idx_d
-        if magd ~= 0:
-            fbd(i,bin+1) = f_pos(beat_index)
-            # set up bin slice to range of expected beats
-            # See freqs from 0 to index 15 - determined from 60kmh (VERIFY)
-            # check if far enough from center
-            if (beat_index>15):
-                bin_slice_u = os_pku(i,beat_index - 15:beat_index)
-            # if not, start from center
-            else:
-                bin_slice_u = os_pku(i,1:beat_index)
-            end
-            # index is index in the subset
-            [magu, idx_u] = max(bin_slice_u)
-            if magu ~= 0:
-                fbu(i,bin+1) = f_pos(beat_index - 15 + idx_u)
-            end
-            
-            # if both not DC
-            if and(fbu(i,bin+1) ~= 0, fbd(i,bin+1)~= 0):
-                fd = -fbu(i,bin+1) + fbd(i,bin+1)
-                fd_array(i,bin+1) = fd/2
-                
-                # if less than max expected and filter clutter doppler
-                if ((abs(fd/2) < fd_max) && (fd/2 > 400)):
-                    sp_array(i,bin+1) = dop2speed(fd/2,lambda)/2
-                    rg_array(i,bin+1) = beat2range( ...
-                        [fbu(i,bin+1) -fbd(i,bin+1)], k, c)
+			
+			# if both not DC
+			if (fbu[bin] != 0 and fbd[bin] != 0):
+				fd = -fbu[bin] + fbd[bin]
+				fd_array[bin] = fd/2
+				
+				# if less than max expected and filter clutter doppler
+				if ((abs(fd/2) < fd_max) and (fd/2 > 400)):
+					# convert Doppler to speed. fd is twice the Doppler therefore
+					# divide by 2
+					# sp_array[bin] = fd*lmda/4
+					# Note that fbd is now positive
+					rg_array[bin] = c*(fbu[bin] + fbd[bin])/(4*slope)
 
-                    # Angle correction
-                   
-                    # Theta in radians
-                    theta = asin(road_width/rg_array(i,bin+1))*correction_factor
+					# ************* Angle correction *******************
+					# Theta in radians
+					theta = np.arcsin(road_width/rg_array[bin])*correction_factor
 
-                    real_v = dop2speed(fd/2,lambda)/(2*cos(theta))
-                    sp_array_corrected(i,bin+1) = real_v
-                
-    # print(Pfa)
-
-
-    # log scale for display purposes
-    return cfar_res_up, cfar_res_dn, upth, dnth, IQ_UP, IQ_DN
-    # return cfar_res_up, cfar_res_dn, 20*np.log10(upth), 20*np.log10(dnth),\
-    #      20*np.log10(abs(IQ_UP), 10),  20*np.log10(abs(IQ_DN))
+					# real_v = fd*lmda/(8*np.cos(theta))
+					sp_array_corrected[bin] = fd*lmda/(8*np.cos(theta))
+				
+	# print(Pfa)
+	# ********************* Safety Algorithm ***********************************
+	ratio = rg_array/sp_array
+	t_safe = 3
+	if (np.any(ratio<t_safe)):
+		# 1 indicates sweep contained target at unsafe distance
+		# UPDATE: put the ratio/time into array to scale how
+		# safe the turn is
+		safety = min(ratio)
+		# for colour map:
+		safety_inv = t_safe-min(ratio)
+		
+	# log scale for display purposes
+	return cfar_res_up, cfar_res_dn, upth, dnth, IQ_UP, IQ_DN
+	# return cfar_res_up, cfar_res_dn, 20*np.log10(upth), 20*np.log10(dnth),\
+	#      20*np.log10(abs(IQ_UP), 10),  20*np.log10(abs(IQ_DN))
