198b1bd9b986a28b5859b6713f25a6c9d17451a1
dayalannair <nairdayalan@gmail.com>
2022-09-28 12:14:07 +0200

    thresh const calc

--- matlab_lib/pfa_to_threshold_constant.m
+++ matlab_lib/pfa_to_threshold_constant.m
@@ -1,21 +1,26 @@
 % Code to convert the Pfa to the cfar threshold constant
 
 % OS CFAR
-n_pfas = length(1:0.1:5);
 
-pfa = zeros(1,n_pfas);
+
+
 i = 1;
 
 train = 64;
 guard = 4;
 n = train - guard;
-k = train; % rank
-
+k = n; % rank
+sos_vals = 1:0.01:5;
+n_pfas = length(sos_vals);
+pfa = zeros(1,n_pfas);
 % Gamma needs plus one
-for sos = 1:0.1:5
+for sos = 1:0.01:5
     pfa_binomial_coeff = gamma(n+1)/(gamma(k+1)*gamma(n-k+1));
+%     disp(pfa_binomial_coeff)
     pfa_numerator = gamma(k-1+1)*gamma(sos+n-k+1);
+%     disp(pfa_numerator)
     pfa_denominator = gamma(sos+n+1);
+%     disp(pfa_denominator)
     pfa(i) = k*pfa_binomial_coeff*pfa_numerator/pfa_denominator;
 %     Pfa = k*gamma(N)/(gamma(k)*gamma(N-k))* gamma(k-1)*gamma(self.SOS+N-k)/gamma(self.SOS+N)
     i = i + 1;
@@ -25,4 +30,9 @@ end
 
 close all
 figure
-plot(pfa)
+plot(sos_vals, pfa)
+
+
+
+
+
