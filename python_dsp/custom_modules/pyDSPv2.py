43412ae5cfd574164bf46be6c3893eaaa20b310d
dayalannair <nairdayalan@gmail.com>
2022-10-23 09:37:30 +0200

    wip on storing processed data

--- python_dsp/custom_modules/pyDSPv2.py
+++ python_dsp/custom_modules/pyDSPv2.py
@@ -62,17 +62,17 @@ def py_trig_dsp(i_data, q_data, twin, n_fft, num_nul, half_train, half_guard, ra
 	# sp_array_corrected = np.zeros(nbins)
 	# beat_arr = np.zeros(nbins)
 
-	safety_inv = 0
-	safety = 0
-	beat_min = 0
+	# safety_inv = 0
+	# safety = 0
+	# beat_min = 0
 	beat_index = 0
 	slope = 2.4e11
 	c = 299792458
-	
 	lmda = 0.0125
 	road_width = 2
 	correction_factor = 3
 	fd_max = 2.6667e3 # for max speed = 60km/h
+	
 	# ********************* beat extraction for multiple targets **************************
 	for bin in range(nbins):
 		# find beat in bin
@@ -131,9 +131,126 @@ def py_trig_dsp(i_data, q_data, twin, n_fft, num_nul, half_train, half_guard, ra
 		# safe the turn is
 		safety = min(ratio)
 		# for colour map:
-		safety_inv = 3-min(ratio)
+		# safety_inv = 3-min(ratio)
 		
 	# log scale for display purposes
 	return os_pku, os_pkd, upth, dnth, IQ_UP, IQ_DN, safety_inv, beat_index, beat_min, rg_array, sp_array
+	# return rg_array, sp_array, safety
 	# return cfar_res_up, cfar_res_dn, 20*np.log10(upth), 20*np.log10(dnth),\
 	#      20*np.log10(abs(IQ_UP), 10),  20*np.log10(abs(IQ_DN))
+
+
+def range_speed_safety(i_data, q_data, twin, n_fft, num_nul, half_train, half_guard, rank, nbins, bin_width, f_ax):
+
+	# SQUARE LAW DETECTOR
+	# NOTE: last element in slice not included
+	iq_u = np.power(i_data[  0:200],2) + np.power(q_data[  0:200],2)
+	iq_d = np.power(i_data[200:400],2) + np.power(q_data[200:400],2)
+
+	# TAYLOR WINDOW
+	# SLL specified as positive
+	iq_u = np.multiply(iq_u, twin)
+	iq_d = np.multiply(iq_d, twin)
+
+	# 512-point FFT
+	IQ_UP = fft(iq_u,n_fft)
+	IQ_DN = fft(iq_d,n_fft)
+
+	# Halve FFTs
+	# note: python starts from zero for this!
+	IQ_UP = IQ_UP[0:round(n_fft/2)]
+	IQ_DN = IQ_DN[round(n_fft/2):]
+
+	# print(len(IQ_UP))   
+	# Null feedthrough
+	# note: python starts from zero for this!
+	IQ_UP[0:num_nul-1] = 0
+	IQ_DN[len(IQ_DN)-num_nul:] = 0
+	# print(len(IQ_UP))
+	IQ_DN = np.flip(IQ_DN)
+	
+	# note the abs
+	SOS = 2
+	# -------------------- CFAR detection ---------------------------
+	cfar_scale = 1 # additional scaling factor
+	os_pku, _ = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_UP), cfar_scale)
+	os_pkd, _ = os_cfar(half_train, half_guard, rank, SOS, abs(IQ_DN), cfar_scale)
+	
+	fbu = np.zeros(nbins)
+	fbd = np.zeros(nbins)
+
+	rg_array = np.zeros(nbins)
+	sp_array = np.zeros(nbins)
+	ratio = np.zeros(nbins)
+
+	# safety_inv = 0
+	# safety = 0
+	# beat_min = 0
+	beat_index = 0
+	slope = 2.4e11
+	c = 299792458
+	lmda = 0.0125
+	road_width = 2
+	correction_factor = 3
+	fd_max = 2.6667e3 # for max speed = 60km/h
+	
+	# ********************* beat extraction for multiple targets **************************
+	for bin in range(nbins):
+		# find beat in bin
+		bin_slice_d = os_pkd[bin*bin_width:(bin+1)*bin_width]
+		magd = np.amax(bin_slice_d)
+		idx_d = np.argmax(bin_slice_d)
+		
+		beat_index = bin*bin_width + idx_d
+		if magd != 0:
+			fbd[bin] = f_ax[beat_index]
+			# set up bin slice to range of expected beats
+			# See freqs from 0 to index 15 - determined from 60kmh (VERIFY)
+			# check if far enough from center
+			if (beat_index>15):
+				beat_min = beat_index - 15
+				bin_slice_u = os_pku[beat_index - 15:beat_index]
+			# if not, start from center
+			else:
+				beat_min = 1
+				bin_slice_u = os_pku[1:beat_index]
+				
+			# index is index in the subset
+			magu = np.amax(bin_slice_u)
+			idx_u = np.argmax(bin_slice_u)
+			if magu != 0:
+				fbu[bin] = f_ax[beat_index - 15 + idx_u]
+
+			
+			# if both not DC
+			if (fbu[bin] != 0 and fbd[bin] != 0):
+				fd = -fbu[bin] + fbd[bin]
+				# fd_array[bin] = fd/2
+				
+				# if less than max expected and filter clutter doppler
+				if ((abs(fd/2) < fd_max) and (fd/2 > 400)):
+					# convert Doppler to speed. fd is twice the Doppler therefore
+					# divide by 2
+					# sp_array[bin] = fd*lmda/4
+					# Note that fbd is now positive
+					rg_array[bin] = c*(fbu[bin] + fbd[bin])/(4*slope)
+
+					# ************* Angle correction *******************
+					# Theta in radians
+					theta = np.arcsin(road_width/rg_array[bin])*correction_factor
+
+					# real_v = fd*lmda/(8*np.cos(theta))
+					sp_array[bin] = fd*lmda/(8*np.cos(theta))
+				
+	# print(Pfa)
+	# ********************* Safety Algorithm ***********************************
+	np.divide(rg_array,sp_array, ratio, where=sp_array!=0)
+	# t_safe = 3
+	if (np.any(ratio<3)):
+		# 1 indicates sweep contained target at unsafe distance
+		# UPDATE: put the ratio/time into array to scale how
+		# safe the turn is
+		safety = min(ratio)
+
+
+	return rg_array, sp_array, safety
---
+++ python_dsp/dual_urad_usb/processed_data_icps.py
@@ -0,0 +1,321 @@
+import sys
+sys.path.append('../custom_modules')
+
+import uRAD_USB_SDK11
+import serial
+from time import time, sleep, strftime,localtime
+import sys
+from pyDSPv2 import range_speed_safety
+import numpy as np
+import cv2
+from scipy import signal
+import threading
+
+# True if USB, False if UART
+usb_communication = True
+
+try:
+	mode_in = str(sys.argv[1])
+	BW = int(sys.argv[2])
+	Ns = int(sys.argv[3])
+	duration = int(sys.argv[4])
+	t = localtime()
+	now = strftime("%H-%M-%S", t)  
+	fs = 200000
+	# runtime = sweeps*Ns/200000
+	if mode_in == "s":
+		print("********** SAWTOOTH MODE **********")
+		resultsFileName = 'IQ_saw_' + str(BW) + '_' + str(Ns) +  '_' + str(now) + '.txt'
+		mode = 2					
+	elif mode_in == "t":
+		print("********** TRIANGLE MODE **********")
+		resultsFileName = 'IQ_tri_' + str(BW) + '_' + str(Ns) + '_' + str(now) + '.txt'
+		mode = 3	
+	elif mode_in == "d":
+		print("********** DUAL RATE MODE **********")
+		resultsFileName = 'IQ_dual_' + str(BW) + '_' + str(Ns) + '_' + str(now) + '.txt'
+		mode = 4					
+	else: 
+		print("Invalid mode")
+		exit()
+	# print("BW = ",str(BW),"\nNs = ",str(Ns),"\nSweeps = ",str(sweeps))
+	# print("Expected run time (saw): ",str(runtime))
+except: 
+	print("Invalid mode")
+	exit()
+
+# input parameters
+# BW and Ns input as arguments
+f0 = 5						# starting at 24.005 GHz
+I_true = True 				# In-Phase Component (RAW data) requested
+Q_true = True 				# Quadrature Component (RAW data) requested
+
+# Serial Port configuration
+ser1 = serial.Serial()
+if (usb_communication):
+	# ser1.port = 'COM3'
+	ser1.port = '/dev/ttyACM0'
+	ser1.baudrate = 1e6
+else:
+	print("Could not find USB 1 connection.")
+	exit()
+
+ser2 = serial.Serial()
+if (usb_communication):
+	# ser1.port = 'COM3'
+	ser2.port = '/dev/ttyACM1'
+	ser2.baudrate = 1e6
+else:
+	print("Could not find USB 2 connection.")
+	exit()
+
+# Sleep Time (seconds) between iterations
+timeSleep = 5e-3
+
+# Other serial parameters
+ser1.bytesize = serial.EIGHTBITS
+ser1.parity = serial.PARITY_NONE
+ser1.stopbits = serial.STOPBITS_ONE
+ser1.timeout = 1
+
+ser2.bytesize = serial.EIGHTBITS
+ser2.parity = serial.PARITY_NONE
+ser2.stopbits = serial.STOPBITS_ONE
+ser2.timeout = 1
+
+def closeProgram():
+	# switch OFF uRAD
+	return_code1 = uRAD_USB_SDK11.turnOFF(ser1)
+	return_code2 = uRAD_USB_SDK11.turnOFF(ser2)
+	if (return_code1 != 0 or return_code2 != 0):
+		print("ERROR: Ending")
+		exit()
+
+# Open serial port
+try:
+	ser1.open()
+except:
+	print("COM port 1 failed to open")
+	closeProgram()
+
+try:
+	ser2.open()
+except:
+	print("COM port 2 failed to open")
+	closeProgram()
+
+
+# switch ON uRAD
+return_code = uRAD_USB_SDK11.turnON(ser1)
+if (return_code != 0):
+	print("uRAD 1 failed to turn on")
+	closeProgram()
+
+return_code = uRAD_USB_SDK11.turnON(ser2)
+if (return_code != 0):
+	print("uRAD 2 failed to turn on")
+	closeProgram()
+
+
+if (not usb_communication):
+	sleep(timeSleep)
+
+# loadConfiguration uRAD
+return_code = uRAD_USB_SDK11.loadConfiguration(ser1, mode, f0, BW, Ns, 0, 0, 0, 0, 0, 0, 0, 0, I_true, Q_true, 0)
+if (return_code != 0):
+	print("uRAD 1 configuration failed")
+	closeProgram()
+
+return_code = uRAD_USB_SDK11.loadConfiguration(ser2, mode, f0, BW, Ns, 0, 0, 0, 0, 0, 0, 0, 0, I_true, Q_true, 0)
+if (return_code != 0):
+	print("uRAD 2 configuration failed")
+	closeProgram()
+
+if (not usb_communication):
+	sleep(timeSleep)
+
+# Poll radar to test if working correctly
+return_code, results, raw_results = uRAD_USB_SDK11.detection(ser1)
+if (return_code != 0):
+	print("Error requesting data from uRAD 1")
+	closeProgram()
+
+return_code, results, raw_results = uRAD_USB_SDK11.detection(ser2)
+if (return_code != 0):
+	print("Error requesting data from uRAD 2")
+	closeProgram()
+
+print("Radars configured. Initialising threads...")
+
+t_0 = time()
+
+# Generate axes
+n_fft = 512
+fax = np.linspace(0, round(fs/2), round(n_fft/2))
+tsweep = 1e-3
+bw = 240e6
+slope = bw/tsweep
+c = 299792458
+rng_ax = c*fax/(2*slope)
+
+# rg_full = np.zeros(16*sweeps)
+twin = signal.windows.taylor(200, nbar=3, sll=100, norm=False)
+nul_width_factor = 0.04
+num_nul = round((n_fft/2)*nul_width_factor)
+
+# CFAR parameters
+n_samples = 200
+half_guard = n_fft/n_samples
+half_guard = int(np.floor(half_guard/2)*2) # make even
+
+half_train = round(20*n_fft/n_samples)
+half_train = int(np.floor(half_train/2))
+rank = 2*half_train -2*half_guard
+# rank = half_train*2
+Pfa_expected = 15e-3
+# factorial needs integer values
+
+nbins = 16
+bin_width = round((n_fft/2)/nbins)
+
+# tsweep = 1e-3
+# bw = 240e6
+# # can optimise out this calculation
+# slope = bw/tsweep
+fs = 200e3
+
+print("System running...")
+
+# ======================= CAMERAS ================================
+cap1 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(2)
+
+cap1.set(3, 176)
+cap1.set(4, 144)
+
+cap2.set(3, 176)
+cap2.set(4, 144)
+
+sleep(1)
+
+
+ret,frame1 = cap1.read()
+ret,frame2 = cap2.read()
+
+def capture(duration, cap1, cap2):
+	# win_factor = 1
+	win1 = "Win 1"
+	cv2.namedWindow(win1, cv2.WINDOW_NORMAL)    
+	# cv2.resizeWindow(win1, win_factor*320, win_factor*240)
+	cv2.resizeWindow(win1, 320, 240)
+	cv2.moveWindow(win1, 550, 50)  
+
+	win2 = "Win 2"
+	cv2.namedWindow(win2, cv2.WINDOW_NORMAL)     
+	# cv2.resizeWindow(win2, win_factor*320, win_factor*240)
+	cv2.resizeWindow(win2, 320, 240)
+	cv2.moveWindow(win2, 900, 50)  
+	t0 = time()
+	t1 = 0
+	print("Video initialised")
+	while (t1<duration):
+		_,frame1 = cap1.read()
+		_,frame2 = cap2.read()
+		cv2.imshow(win1,cv2.flip(frame1,0))
+		# cv2.imshow(win1,frame1)
+		cv2.imshow(win2,frame2)
+		cv2.waitKey(1)
+		t1 = time() - t0
+	cap1.release()	
+	cap2.release()
+
+vid1 = threading.Thread(target=capture, args=[duration, cap1, cap2])
+
+
+rg_array = np.zeros(10000, dtype=int)
+sp_array = np.zeros(10000, dtype=int)
+sf_array = np.zeros(10000, dtype=int)
+
+rg_array_2 = np.zeros(10000, dtype=int)
+sp_array_2 = np.zeros(10000, dtype=int)
+sf_array_2 = np.zeros(10000, dtype=int)
+
+def dsp_thread_usb(port, radar_index, i):
+	
+	# Global inputs
+	global n_fft
+	global twin
+	global num_nul
+	global half_guard
+	global half_train
+	global f_ax
+	global bin_width
+	global nbins
+	global rank
+
+	# Global outputs
+	global upth_2
+	global dnth_2
+	global fftd_2
+	global fftu_2
+
+	return_code, _, raw_results = uRAD_USB_SDK11.detection(port)
+	if (return_code != 0):
+		closeProgram()
+
+	if radar_index == 0:
+		rg_array[i], sp_array[i], sf_array[i] = range_speed_safety(raw_results[0], \
+			raw_results[1], twin, n_fft, num_nul, half_train, half_guard, rank, nbins, bin_width, f_ax)
+	else:
+		rg_array_2[i], sp_array_2[i], sf_array_2[i] = range_speed_safety(raw_results[0], \
+			raw_results[1], twin, n_fft, num_nul, half_train, half_guard, rank, nbins, bin_width, f_ax)
+
+urad1_index = 0
+urad2_index = 1
+row_index = 0
+fname = "icps_results.txt"
+# urad1 = threading.Thread(target=dsp_thread_usb, args=[ser1, urad1_index, row_index])
+# urad2 = threading.Thread(target=dsp_thread_usb, args=[ser2, urad2_index, row_index])
+try:
+	vid1.start()
+	t0 = time()
+	t1 = 0
+
+	while (t1<duration):
+
+		# MUST BE CALLED AGAIN TO RESTART THE THREAD
+		urad1 = threading.Thread(target=dsp_thread_usb, args=[ser1, urad1_index, row_index])
+		urad2 = threading.Thread(target=dsp_thread_usb, args=[ser2, urad2_index, row_index])
+
+		t0_proc = time()
+		
+		urad1.start()
+		urad2.start()
+		urad1.join()
+		urad2.join()
+
+		row_index = row_index + 1
+		t1_proc = time()-t0_proc
+		t1 = time() - t0
+		
+	vid1.join()
+	# vid2.join()
+	print("Elapsed time: ", str(time()-t_0))
+
+	with open(fname, 'w') as results:
+		for i in range(np.shape())
+		result_string = 
+
+	print("Complete.")
+	cap1.release()
+	cap2.release()
+	uRAD_USB_SDK11.turnOFF(ser1)
+	uRAD_USB_SDK11.turnOFF(ser2)
+
+except KeyboardInterrupt:
+	cap1.release()
+	cap2.release()
+	uRAD_USB_SDK11.turnOFF(ser1)
+	uRAD_USB_SDK11.turnOFF(ser2)
+	print("Interrupted.")
+	exit()
