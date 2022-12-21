9b99f5f3b8f174f5977172a9c76caa2bc80fc55d
dayalannair <nairdayalan@gmail.com>
2022-12-21 19:33:25 +0200

    Updated plotting of DSD outputs. Focus on specific cases and plot full point cloud outputs

--- matlab_sim/ModifiedPrebuiltAdt/plotResults.m
+++ matlab_sim/ModifiedPrebuiltAdt/plotResults.m
@@ -6,18 +6,29 @@
 
 %%
 % data = singleCarLeftToRight;
-measuredPosition = zeros(44,1);
-measuredVelocity = zeros(44,1);
-actualPosition = zeros(44,1);
-actualVelocity = zeros(44,1);
 N = length(data);
+
+measuredPosition1 = zeros(N,1);
+measuredVelocity1 = zeros(N,1);
+measuredPosition2 = zeros(N,1);
+measuredVelocity2 = zeros(N,1);
+
+actualPosition1 = zeros(N,1);
+actualPosition2 = zeros(N,1);
+actualVelocity1 = zeros(N,1);
+actualVelocity2 = zeros(N,1);
+
 t   = zeros(N,1);
 for i = 1:N
     t(i) = data(i).Time;
-    measuredVelocity(i) = data(i).ObjectDetections{1}.Measurement(5);
-    measuredPosition(i) = data(i).ObjectDetections{1}.Measurement(2);
-    actualVelocity(i)   = data(i).ActorPoses(2).Velocity(2);
-    actualPosition(i)   = data(i).ActorPoses(2).Position(2);
+    measuredVelocity1(i) = data(i).ObjectDetections{1}.Measurement(5);
+    measuredPosition1(i) = data(i).ObjectDetections{1}.Measurement(2);
+    measuredVelocity2(i) = data(i).ObjectDetections{2}.Measurement(5);
+    measuredPosition2(i) = data(i).ObjectDetections{2}.Measurement(2);
+    actualVelocity1(i)   = data(i).ActorPoses(2).Velocity(2);
+    actualVelocity2(i)   = data(i).ActorPoses(3).Velocity(2);
+    actualPosition1(i)   = data(i).ActorPoses(2).Position(2);
+    actualPosition2(i)   = data(i).ActorPoses(3).Position(2);
 end
 
 
@@ -25,11 +36,16 @@ close all
 figure
 tiledlayout(2, 1)
 nexttile
-plot(t, measuredVelocity)
+scatter(t, measuredVelocity1)
 hold on
-plot(t, actualVelocity)
+plot(t, actualVelocity1)
+scatter(t, measuredVelocity2)
+plot(t, actualVelocity2)
 % axis([0 max(t) -25 -10])
 nexttile
-plot(t, measuredPosition)
+scatter(t, measuredPosition1)
 hold on
-plot(t, actualPosition)
+plot(t, actualPosition1)
+scatter(t, measuredPosition2)
+hold on
+plot(t, actualPosition2)
