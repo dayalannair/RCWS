fa49f0e790bcbfad802d705185909c9cd9d17e36
dayalannair <nairdayalan@gmail.com>
2023-01-04 18:53:18 +0200

    Prepared ADT fig for report. Need to do for other scenarios

--- matlab_sim/ModifiedPrebuiltAdt/plotResults.m
+++ matlab_sim/ModifiedPrebuiltAdt/plotResults.m
@@ -13,10 +13,10 @@ measuredVelocity1 = NaN(N,15);
 measuredPosition2 = NaN(N,15);
 measuredVelocity2 = NaN(N,15);
 
-actualPosition1 = NaN(N,15);
-actualPosition2 = NaN(N,15);
-actualVelocity1 = NaN(N,15);
-actualVelocity2 = NaN(N,15);
+actualPosition1 = NaN(15, 1);
+actualPosition2 = NaN(15, 1);
+actualVelocity1 = NaN(15, 1);
+actualVelocity2 = NaN(15, 1);
 
 t   = NaN(N,1);
 for i = 1:N
@@ -70,36 +70,77 @@ end
 
 %% Plots
 close all
-fig = figure('WindowState','maximized');
+figure1 = figure('WindowState','maximized');
 tiledlayout(2, 2)
 nexttile
 hold on
-scatter(t, abs(measuredVelocity2)*3.6, 'Marker','.')
-plot(t, abs(actualVelocity2)*3.6)
-title("LHS Velocity Measurements")
+scatter(t, abs(measuredVelocity2(:, 5:11))*3.6, 70,'Marker','.')
+p1 = plot(t, abs(actualVelocity2)*3.6, 'DisplayName', 'Actual');
+title("LHS Radar Velocity Measurements")
 xlabel("Time (s)")
 ylabel("Speed (km/h)")
+axis([0 max(t) 0 80])
+legend(p1)
 
 nexttile
 hold on
-scatter(t, abs(measuredVelocity1)*3.6, 'Marker','.')
-plot(t, abs(actualVelocity1)*3.6)
-title("RHS Velocity Measurements")
+scatter(t, abs(measuredVelocity1(:, 1:5))*3.6,70, 'Marker','.')
+p2 = plot(t, abs(actualVelocity1)*3.6, 'DisplayName', 'Actual');
+title("RHS Radar Velocity Measurements")
 xlabel("Time (s)")
 ylabel("Speed (km/h)")
-% axis([0 max(t) -25 -10])
-nexttile
-hold on
-scatter(t, abs(measuredPosition2), 'Marker','.')
-plot(t, abs(actualPosition2))
-title("LHS Range Measurements")
-xlabel("Time (s)")
-ylabel("Range (m)")
+axis([0 max(t) 0 80])
+legend(p2)
 
 nexttile
 hold on
-scatter(t, abs(measuredPosition1), 'Marker','.')
-plot(t, abs(actualPosition1))
-title("RHS Range Measurements")
+scatter(t, abs(measuredPosition2(:, 5:11)),70, 'Marker','.')
+p3 = plot(t, abs(actualPosition2), 'DisplayName', 'Actual');
+title("LHS Radar Range Measurements")
 xlabel("Time (s)")
 ylabel("Range (m)")
+legend(p3)
+
+nexttile
+hold on
+scatter(t, abs(measuredPosition1(:, 1:5)), 70, 'Marker','.')
+p4 = plot(t, abs(actualPosition1), 'DisplayName', 'Actual');
+title("RHS Radar Range Measurements")
+xlabel("Time (s)")
+ylabel("Range (m)")
+legend(p4)
+
+
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.148958333333333 0.203125],...
+    [0.789598290598291 0.873931623931624],'String',{'Car 1'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.358854166666667 0.323958333333333],...
+    [0.75434188034188 0.841880341880342],'String',{'Car 2'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.213541666666667 0.189583333333333],...
+    [0.359042735042735 0.27991452991453],'String',{'Car 1'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.634895833333333 0.684895833333333],...
+    [0.757547008547009 0.870726495726496],'String',{'Car 2'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.799479166666667 0.771875],...
+    [0.776777777777778 0.862179487179487],'String',{'Car 1'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.3296875 0.359895833333333],...
+    [0.345153846153846 0.264957264957265],'String',{'Car 2'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.7 0.657291666666667],...
+    [0.36865811965812 0.286324786324786],'String',{'Car 2'});
+
+% Create textarrow
+annotation(figure1,'textarrow',[0.768233387358185 0.80226904376013],...
+    [0.301245250431779 0.250431778929188],'String',{'Car 1'});
+
