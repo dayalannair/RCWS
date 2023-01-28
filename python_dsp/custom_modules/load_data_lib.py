e0a9167d419b4a84e54d8f6803be9e2562ddc50e
dayalannair <nairdayalan@gmail.com>
2023-01-28 17:51:32 +0200

    attempted plotting another set and doing fancy things with path init. ran into error in result proc

--- python_dsp/custom_modules/load_data_lib.py
+++ python_dsp/custom_modules/load_data_lib.py
@@ -43,9 +43,12 @@ def load_data():
 
 
 def load_dual_data():
-    
-    file_path_lhs = Path(r"C:\Users\naird\OneDrive - University of Cape Town\RCWS_DATA\road_data_03_11_2022\iq_data\lhs_iq_12_18_12.txt")
-    file_path_rhs = Path(r"C:\Users\naird\OneDrive - University of Cape Town\RCWS_DATA\road_data_03_11_2022\iq_data\rhs_iq_12_18_12.txt")
+    # folder = "road_data_03_11_2022"
+    folder = "road_data_05_11_2022"
+    # time = "12_18_12"
+    time = "11_01_36"
+    file_path_lhs = Path(r"C:\\Users\\naird\\OneDrive - University of Cape Town\\RCWS_DATA\\road_data_05_11_2022\\iq_data\\lhs_iq_11_01_36.txt")
+    file_path_rhs = Path(r"C:\\Users\\naird\\OneDrive - University of Cape Town\\RCWS_DATA\\road_data_05_11_2022\\iq_data\\rhs_iq_11_01_36.txt")
 
     # subset = range(0, 4000)
     subset = range(0, 5000)
