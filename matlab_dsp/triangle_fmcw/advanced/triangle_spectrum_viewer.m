c7ee2c278d01445378bde44a0558256ff8b1da77
dayalannair <nairdayalan@gmail.com>
2023-03-12 09:50:41 +0200

    Tested normalising FFT. Improved script for bulk rotation of rhs video

--- matlab_dsp/triangle_fmcw/advanced/triangle_spectrum_viewer.m
+++ matlab_dsp/triangle_fmcw/advanced/triangle_spectrum_viewer.m
@@ -1,109 +1,61 @@
 %{
-%% PLOT TRIANGLE FMCW SPECTRA
+%% PLOT TRIANGLE FMCW BEAT SPECTRA
 Script to plot only the spectra after application of a window function and
 an N-point FFT
 %}
-
 % Import data and parameters
-subset = 900:1100;
+% subset = 900:1100;
+subset = 1:2700;
 addpath('../../../matlab_lib/');
 addpath(['../../../../../OneDrive - ' ...
     'University of Cape Town/RCWS_DATA/car_driveby/']);
+addpath(['../../../../../OneDrive - University of Cape Town/' ...
+    'RCWS_DATA/road_data_03_03_2023/iq_data/']);
 % Import video
 addpath(['../../../../../OneDrive - ' ...
     'University of Cape Town/RCWS_DATA/videos/']);
 n_samples = 200;
-% Taylor Window
+
 nbar = 3;
-sll = -100;
+sll = -20;
 win = taylorwin(n_samples, nbar, sll);
-% win = hamming(n_samples);
+
 [fc, c, lambda, tm, bw, k, iq_u, iq_d, t_stamps] = ...
     import_data(subset, win.');
 n_sweeps = size(iq_u,1);
 
-% ======================== Tunable parameters =============================
-% These determine the system detection performance
-% nbar = 3;
-% sll = -200;
-% F = 1e-3;
 n_fft = 512;
-% n_interp = 2*n_fft;
-% =========================================================================
 
-% Taylor Window
-% win = taylorwin(n_samples, nbar, sll);
-% win = hamming(n_samples);
-% 
-% iq_u = iq_u.*win.';
-% iq_d = iq_d.*win.';
-
-% FFT
-nul_width_factor = 0.04;
-num_nul = round((n_fft/2)*nul_width_factor);
-
+% FFT - note that true value is normalised by dividing by Ns
 FFT_U = fft(iq_u,n_fft,2);
 FFT_D = fft(iq_d,n_fft,2);
-
-% FFT_U = interpft(FFT_U,n_interp,2);
-% FFT_D = interpft(FFT_D,n_interp,2);
 
 % Halve FFTs
 FFT_U = FFT_U(:, 1:n_fft/2);
 FFT_D = FFT_D(:, n_fft/2+1:end);
 
-% Null feedthrough
-% METHOD 1: slice
-FFT_U(:, 1:num_nul) = 0;
-FFT_D(:, end-num_nul+1:end) = 0;
-
-% METHOD 2: Remove average
-% DEPRECATED - remove avg in IQ
-% IQ_UP2 = FFT_U - mean(FFT_U,2);
-% IQ_DN2 = FFT_D - mean(FFT_D,2);
-
-% flip
+% Flip negative half of down chirp spectrum
 FFT_D = flip(FFT_D,2);
-% IQ_DN2 = flip(IQ_DN2,2);
-%%
+
 FFT_U = absmagdb(FFT_U);
 FFT_D = absmagdb(FFT_D);
 
-% ax_dims = [0 round(n_interp/2) 60 160];
-% ax_dims = [0 round(n_fft/2) 30 140];
-sweep = 1;
+ax_dims = [0 round(n_fft/2) -74 26];
+
 close all
 fig1 = figure('WindowState','maximized');
 movegui(fig1,'east')
 tiledlayout(2,1)
-nexttile
-p1 = plot(FFT_U(sweep,:));
-title("UP chirp positive half")
-% axis(ax_dims)
-nexttile
-p2 = plot(FFT_D(sweep,:));
-title("DOWN chirp flipped negative half")
-% axis(ax_dims)
-% nexttile
-% plot(absmagdb(IQ_UP2(sweep,:)))
-% title("UP chirp positive half average nulling")
-% axis(ax_dims)
-% nexttile
-% plot(absmagdb(IQ_DN2(sweep,:)))
-% title("DOWN chirp flipped negative half average nulling")
-% axis(ax_dims)
-%     nexttile
-%     plot(abs(IQ_UP2(sweep,:)))
-%     title("UP chirp positive half average nulling")
-%     axis([0 256 0 7e6])
-% %     yline(125)
-%     nexttile
-%     plot(abs(IQ_DN2(sweep,:)))
-%     title("DOWN chirp flipped negative half average nulling")
-%     axis([0 256 0 7e6])
-% %     yline(125)
 
-%     pause(0.1)
+nexttile
+p1 = plot(FFT_U(1,:));
+title("UP chirp positive half")
+axis(ax_dims)
+
+nexttile
+p2 = plot(FFT_D(1,:));
+title("DOWN chirp flipped negative half")
+axis(ax_dims)
 
 for i = 1:n_sweeps
     set(p1, 'YData',FFT_U(i,:))
--- matlab_lib/import_data.m
+++ matlab_lib/import_data.m
@@ -2,6 +2,14 @@
 %% IMPORT RECORDED DATA
 Imports recorded data, removes DC offset by subtracting the mean, and
 applies a window function after converting data to complex samples
+
+NOTE: Path must be added in the program calling this function
+i.e. path relative to the call script so may need more or less../
+addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/trig_fmcw_data/');
+addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/trolley_test/');
+addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/m4_rustenberg/');
+addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/office/');
+
 %}
 function [fc, c, lambda, tm, bw, k, iq_u, iq_d, t_stamps] = ...
 import_data(sweeps, windowCoeffs)
@@ -13,17 +21,10 @@ import_data(sweeps, windowCoeffs)
     bw = 240e6;                     % Bandwidth
     k = bw/tm;                      % Sweep slope
     % Data
-    % NOTE: Path must be added in the program calling this function
-    % i.e. path is relative to the call function so may need more or less
-    % ../
-% addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/trig_fmcw_data/');
-% addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/trolley_test/');
-% addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/m4_rustenberg/');
-% addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/office/');
-addpath('../../../../OneDrive - University of Cape Town/RCWS_DATA/road_data_03_03_2023/iq_data/');
-iq_tbl=readtable('rhs_iq_12_57_07.txt','Delimiter' ,' ');
 
-% iq_tbl=readtable('IQ_usb.txt','Delimiter' ,' ');
+% iq_tbl=readtable('rhs_iq_12_57_07.txt','Delimiter' ,' ');
+iq_tbl=readtable('rhs_iq_12_57_50.txt','Delimiter' ,' ');
+
 % iq_tbl=readtable('IQ_tri_20kmh.txt','Delimiter' ,' ');
 % iq_tbl=readtable('IQ_tri_30kmh.txt','Delimiter' ,' ');
 % iq_tbl=readtable('IQ_tri_40kmh.txt','Delimiter' ,' ');
@@ -31,71 +32,27 @@ iq_tbl=readtable('rhs_iq_12_57_07.txt','Delimiter' ,' ');
 % iq_tbl=readtable('IQ_tri_60kmh.txt','Delimiter' ,' ');
 % iq_tbl=readtable('IQ_tri_70kmh.txt','Delimiter' ,' ');
 
-% iq_tbl=readtable('IQ_tri_240_200_12-02-23.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_07-31-53.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_0_8192_sweeps.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_2022-07-08 11-16-07.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_2022-07-08 11-17-09.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_2022-07-08 11-18-14.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_2022-07-08 11-19-11.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_2022-07-08 11-20-05.txt','Delimiter' ,' ');
-% iq_tbl=readtable('IQ_tri_240_200_2022-07-08 11-20-57.txt','Delimiter' ,' ');
-
-%     t_stamps = iq_tbl.Var801;
+    % Split data
     t_stamps = [];
     i_up = table2array(iq_tbl(sweeps,1:200));
     i_dn = table2array(iq_tbl(sweeps,201:400));
     q_up = table2array(iq_tbl(sweeps,401:600));
     q_dn = table2array(iq_tbl(sweeps,601:800));
     
-    % Filter DC
+    % Compute voltage intervals
     max_voltage = 3.3;
 	ADC_bits = 12;
 	ADC_intervals = 2^ADC_bits;
     vinv = max_voltage/ADC_intervals;
-% 
+    
+    % Subtract the DC offset
     i_up = i_up*vinv - mean(i_up*vinv, 2);
     i_dn = i_dn*vinv - mean(i_dn*vinv, 2);
     q_up = q_up*vinv - mean(q_up*vinv, 2);
     q_dn = q_dn*vinv - mean(q_dn*vinv, 2);
 
-    i_up = i_up*vinv - mean(i_up*vinv, 1);
-    i_dn = i_dn*vinv - mean(i_dn*vinv, 1);
-    q_up = q_up*vinv - mean(q_up*vinv, 1);
-    q_dn = q_dn*vinv - mean(q_dn*vinv, 1);
-
-
-
     % Create complex number and apply window coefficients
-%     iq_u = (i_up + 1i*q_up).*windowCoeffs;
-%     iq_d = (i_dn + 1i*q_dn).*windowCoeffs;
+    iq_u = (i_up + 1i*q_up).*windowCoeffs;
+    iq_d = (i_dn + 1i*q_dn).*windowCoeffs;
 
-    iq_u = i_up.*windowCoeffs + 1i.*(q_up.*windowCoeffs);
-    iq_d = i_dn.*windowCoeffs + 1i.*(q_dn.*windowCoeffs);
-
-
-    % Square Law detector
-    % NOTE: will result in a real FFT
-
-    % Option 1 - the most correct
-    % same as (I + jQ)*win
-% 
-%     iq_u = (i_up.*windowCoeffs).^2 + (q_up.*windowCoeffs).^2;
-%     iq_d = (i_dn.*windowCoeffs).^2 + (q_dn.*windowCoeffs).^2;
-
-% 
-%     iq_u = (i_up.*windowCoeffs).^2 + q_up.^2;
-%     iq_d = (i_dn.*windowCoeffs).^2 + q_dn.^2;
-
-% 
-%     iq_u = (i_up.^2 + q_up.^2).*windowCoeffs;
-%     iq_d = (i_dn.^2 + q_dn.^2).*windowCoeffs;
-
-%     iq_u = (i_up.^2 + (q_up.*windowCoeffs).^2);
-%     iq_d = (i_dn.^2 + (q_dn.*windowCoeffs).^2);
-
-
-    % Normal
-%     iq_u= i_up + 1i*q_up;
-%     iq_d = i_dn + 1i*q_dn;
    return
