more testing and updating of plots. Average V is correct

python_dsp/custom_modules/pyDSPv2.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 # NOTE: Range, speed, and possibly safety results of the below are not correct
 def py_trig_dsp(i_data, q_data, windowCoeffs, n_fft, half_train, half_guard, \
-		nbins, bin_width, fpos, fneg, SOS, calib, scan_width, angOffsetMinRange, angOffset, NVL):
+		nbins, bin_width, fpos, fneg, SOS, calib, scan_width, \
+			angOffsetMinRange, angOffset, NVL, road_width):
 
 
 	i_u = np.subtract(np.multiply(i_data[  0:200], NVL), np.mean(np.multiply(i_data[  0:200], NVL)))
@@ -96,13 +97,6 @@ def py_trig_dsp(i_data, q_data, windowCoeffs, n_fft, half_train, half_guard, \
 	slope = 2.4e11
 	c = 299792458
 	lmda = 0.0125
-	road_width = 2
-	correction_factor = 1
-	fd_max = 2.6667e3 # for max speed = 60km/h
-	# magu = 0
-	# idx_u = 0
-	# magd = 0
-	# idx_d = 0
 	# ********************* beat extraction for multiple targets **************************
 	for bin in range(nbins):
 		# find beat frequency in bin of down chirp
@@ -153,7 +147,10 @@ def py_trig_dsp(i_data, q_data, windowCoeffs, n_fft, half_train, half_guard, \
 				# if target moving towards radar
 				if (fbu[bin] < fbd[bin]):
 				# if (fbu[bin] > 0 and fbd[bin] > 0):
-					fd = (-fbu[bin] + fbd[bin])*calib/2
+
+					# div by 4: first division by 2 is for beat freq, 
+					# second is for dop to vel
+					fd = (-fbu[bin] + fbd[bin])*calib/4 
 					if (fd>800): # NOTE: fmin = 800 Hz
 						# fd_array[bin] = fd/2
 						
@@ -161,17 +158,18 @@ def py_trig_dsp(i_data, q_data, windowCoeffs, n_fft, half_train, half_guard, \
 						# if ((abs(fd/2) < fd_max) and (fd/2 > 400)):
 						# convert Doppler to speed. fd is twice the Doppler therefore
 						# divide by 2
-						sp_array[bin] = fd*lmda/2
+						# sp_array[bin] = fd*lmda/2
 						# Note that fbd is now positive
 						rg_array[bin] = c*(fbu[bin] + fbd[bin])/(4*slope)*calib
 
-						# to account for angle offset on left radar. for right radar, set angOffsetMinRange = 100
-						# if rg_array[bin] > angOffsetMinRange:
-						# 	sp_array[bin] = fd*lmda/(np.cos(angOffset - np.arcsin(road_width/rg_array[bin])))
+						# to account for angle offset on left radar. 
+						# for right radar, set angOffsetMinRange = 100
+						if rg_array[bin] > angOffsetMinRange:
+							sp_array[bin] = fd*lmda/(np.cos(angOffset - np.arcsin(road_width/rg_array[bin])))
 						
-						# # Else ignore/dont correct for left and calculate as normal for right
-						# else:
-						# 	sp_array[bin] = fd*lmda/(np.cos(np.arcsin(road_width/rg_array[bin])))
+						# Else ignore/dont correct for left and calculate as normal for right
+						else:
+							sp_array[bin] = fd*lmda/(np.cos(np.arcsin(road_width/rg_array[bin])))
 	
 
 

python_dsp/offline_dsp/offline_mono_plot_results.py
@@ -30,11 +30,15 @@ half_guard = 14
 Pfa = 1e-4
 nbins = 32
 scan_width = 32
-calib = 0.98
+calib = 0.9837
 ns = 200
 
-# Left radar angle adjustment
-angOffsetMinRange = 7.1 
+# Right radar angle correction
+rhs_road_width = 1.5
+angOffsetMinRange = 100 
+
+# Left radar angle adjustment and correction
+# angOffsetMinRange = 7.1 
 angOffset = 25*np.pi/180
 
 # DC cancellation
@@ -102,9 +106,10 @@ for sweep in subset:
 	# half_guard, nbins, bin_width, f_ax, SOS)
 
 
-	_,_,_,_,_,_,_,_,rgMtx[i, :], spMtx[i, :], sfVector[i] = py_trig_dsp(i_data,q_data, win, n_fft, half_train, \
+	_,_,_,_,_,_,_,_,rgMtx[i, :], spMtx[i, :], sfVector[i] = \
+		py_trig_dsp(i_data,q_data, win, n_fft, half_train, \
 	half_guard, nbins, bin_width, fpos, fneg, SOS, calib, scan_width, angOffsetMinRange, \
-	angOffset, numVoltageLevels)
+	angOffset, numVoltageLevels, rhs_road_width)
 
 
 	
@@ -163,7 +168,7 @@ plt.tight_layout()
 # plt.show()
 timeStampsTrimmed = timeStamps[0:len_subset]
 rngAxBins = np.linspace(0, np.max(rngAxNeg), nbins)
-plt.imshow(spMtx,cmap='gist_ncar', origin='upper', vmin=0, vmax=70, aspect='auto', \
+plt.imshow(spMtx,cmap='terrain_r', origin='upper', vmin=0, vmax=70, aspect='auto', \
 		     interpolation='none', extent=[0, 62.5, np.max(timeStampsTrimmed), 0]) #, extent=[0, 62.5, 0, len_subset]
 # line3, = ax[1].plot(timeStampsTrimmed , sfVector)
 # thismanager = get_current_fig_manager()

python_dsp/offline_dsp/plot_offline_results.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append('../custom_modules')
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy import stats
 # Parameters
 fs = 200e3
 n_fft = 1024
@@ -29,27 +29,60 @@ slope = bw/tsweep
 rngAxPos = c*fpos/(2*slope)
 rngAxNeg = c*fneg/(2*slope)
 
-rng_fname = "range_results.txt"
-spd_fname = "speed_results.txt"
-spMtx = np.loadtxt(spd_fname,  delimiter=' ')
-
+# rng_fname1 = "range_results.txt"
+spd_fname1 = "speed_results_20kmph.txt"
+# rng_fname2 = "range_results.txt"
+spd_fname2 = "speed_results_50kmph.txt"
+# rng_fname3 = "range_results.txt"
+spd_fname3 = "speed_results_60kmph.txt"
+spMtx1 = np.loadtxt(spd_fname1,  delimiter=' ')
+# rgMtx1 = np.loadtxt(rng_fname1,  delimiter=' ')
+spMtx2 = np.loadtxt(spd_fname2,  delimiter=' ')
+# rgMtx2 = np.loadtxt(rng_fname2,  delimiter=' ')
+spMtx3 = np.loadtxt(spd_fname3,  delimiter=' ')
+# rgMtx3 = np.loadtxt(rng_fname3,  delimiter=' ')
 
 print("Processing Complete. Displaying results...")
 
 plt.tight_layout()
 # 30-second burst
-duration = 30
-timeAxTicks = np.linspace(0, 30, 14)
-rngAxTicks = np.linspace(0, np.max(rngAxNeg), 15)
-plt.imshow(spMtx, origin='upper', vmin=0, vmax=70, aspect='auto', \
-		     interpolation='none', extent=[0, 62.5, 30, 0], cmap='terrain_r') #, extent=[0, 62.5, 0, len_subset]
-plt.colorbar()
+# duration = 30
 
+# Sweeps only (deprecated)
+# using update rate approx 92 Hz, and 4000 sweeps acquired,
+# duration was either set to 45 seconds or sweeps were set to 4000
+# Seems to be the later
+lenSubset = np.shape(spMtx1)[0] 
+duration = lenSubset/92
+
+timeAxTicks = np.linspace(0, duration, 20)
+rngAxTicks = np.linspace(0, np.max(rngAxNeg), 15)
+plt.imshow((spMtx1+spMtx2+spMtx3), origin='upper', vmin=0, vmax=70, aspect='auto', \
+		     interpolation='none', extent=[0, 62.5, duration, 0], cmap='terrain_r') #, extent=[0, 62.5, 0, len_subset]
+# plt.imshow(spMtx2, origin='upper', vmin=0, vmax=70, aspect='auto', \
+# 		     interpolation='none', extent=[0, 62.5, duration, 0], cmap='terrain_r') #, extent=[0, 62.5, 0, len_subset]
+# plt.imshow(spMtx3, origin='upper', vmin=0, vmax=70, aspect='auto', \
+# 		     interpolation='none', extent=[0, 62.5, duration, 0], cmap='terrain_r') #, extent=[0, 62.5, 0, len_subset]
+plt.colorbar()
+plt.grid()
 plt.xticks(rngAxTicks)
 plt.yticks(timeAxTicks)
 # line3, = ax[1].plot(timeStampsTrimmed , sfVector)
 # thismanager = get_current_fig_manager()
 # thismanager.window.SetPosition((500, 0))
+spMtxNoZeros = spMtx1[spMtx1 != 0]
+# rgMtxNoZeros = rgMtx1[rgMtx1 != 0]
+
+# distance = np.max(rgMtxNoZeros) - np.min(rgMtxNoZeros)
+# t0 = np.argmax(rgMtxNoZeros)
+# t1 = np.argmin(rgMtxNoZeros)
+# deltaT = 
+# print(t0)
+# print(t1)
+# print(spMtxNoZeros)
+print(np.average(spMtxNoZeros, axis=None))
+print(stats.mode(spMtxNoZeros, axis=None)[0])
+
 
 plt.xlabel("Range (m)")
 plt.ylabel("Time (s)")