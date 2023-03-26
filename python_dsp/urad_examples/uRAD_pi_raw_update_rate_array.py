dd404522788b65e64a52c58718fae6ad65377a83
dayalannair <nairdayalan@gmail.com>
2023-03-26 16:17:59 +0200

    added scripts to compare update rate of uRAD v1.1 Pi and v1.2 USB and to compare array vs. write to file

---
+++ python_dsp/urad_examples/uRAD_pi_raw_update_rate_array.py
@@ -0,0 +1,62 @@
+
+import sys
+sys.path.append('../custom_modules')
+import uRAD_RP_SDK10		# uRAD v1.1 RPi lib
+from time import time, sleep
+
+# input parameters
+mode = 3					# triangular. Try dual rate mode as well
+f0 = 5						# start frequency 24.005 GHz
+BW = 240					# sweep 240 MHz
+Ns = 100					# 200 samples
+Ntar = 1					# 2 target of interest
+Rmax = 100					# searching along the full distance range
+MTI = 0						# MTI mode enabled: only want information on moving targets
+Mth = 1						# MTI threshold. Sets sensitivity with 4 being highest. Vary: find optimal for scenario
+Alpha = 10					# signal has to be 10 dB higher than its surrounding. may need tuning
+distance_true = False 		# request distance information
+velocity_true = False		# request velocity information
+SNR_true = False 			# Signal-to-Noise-Ratio information requested
+I_true = True 				# In-Phase Component (RAW data) not requested
+Q_true = True 				# Quadrature Component (RAW data) not requested
+movement_true = False 		# not interested in boolean movement detection
+
+#Switch on Pi uRAD
+uRAD_RP_SDK10.turnON()
+
+I = [0] * 2 * Ns
+Q = [0] * 2 * Ns
+# no return code from SDK 1.0 for RPi
+uRAD_RP_SDK10.loadConfiguration(mode, f0, BW, Ns, Ntar, Rmax, MTI, Mth)
+
+# I_file = open('I.txt', 'w')
+# Q_file = open('Q.txt', 'w')
+iterations = 0
+t_0 = time()
+print("Detecting\n")
+while True:
+	try:
+		t_i = time()
+
+		uRAD_RP_SDK10.detection(0, 0, 0, I, Q, 0)
+		iterations += 1
+
+		if (iterations > 100):
+			print('Fs %1.2f Hz' % (iterations/(t_i-t_0)))
+		# I_string = ''
+		# Q_string = ''
+		# for index in range(len(I)):
+		# 	I_string += '%d ' % I[index]
+		# for index in range(len(Q)):
+		# 	Q_string += '%d ' % Q[index]
+
+		# I_file.write(I_string + '%1.3f\n' % t)
+		# Q_file.write(Q_string + '%1.3f\n' % t)
+
+	except KeyboardInterrupt:
+		print("Exiting gracefully\n")
+		uRAD_RP_SDK10.turnOFF()
+		# I_file.close()
+		# Q_file.close()
+		exit()
+
---
+++ python_dsp/urad_examples/uRAD_usb_raw_update_rate_array.py
@@ -0,0 +1,107 @@
+import sys
+sys.path.append('../custom_modules')
+import uRAD_USB_SDK11		# import uRAD libray
+import serial
+from time import time, sleep
+
+# True if USB, False if UART
+usb_communication = True
+
+# input parameters
+mode = 2					# sawtooth mode
+f0 = 5						# starting at 24.005 GHz
+BW = 240					# using all the BW available = 240 MHz
+Ns = 200					# 200 samples
+Ntar = 3					# Don't apply as only raw data is desired
+Rmax = 100					# Don't apply as only raw data is desired
+MTI = 0						# MTI mode disable because we want information of static and moving targets
+Mth = 0						# Don't apply as only raw data is desired
+Alpha = 10					# Don't apply to raw signals
+distance_true = False 		# Don't request distance information
+velocity_true = False		# Don't request velocity information
+SNR_true = False 			# Don't request Signal-to-Noise-Ratio information
+I_true = True 				# In-Phase Component (RAW data) requested
+Q_true = True 				# Quadrature Component (RAW data) requested
+movement_true = False 		# Don't apply as only raw data is desired
+
+# Serial Port configuration
+ser = serial.Serial()
+if (usb_communication):
+	ser.port = 'COM1'
+	ser.baudrate = 1e6
+else:
+	ser.port = '/dev/serial0'
+	ser.baudrate = 115200
+
+# Sleep Time (seconds) between iterations
+timeSleep = 5e-3
+
+# Other serial parameters
+ser.bytesize = serial.EIGHTBITS
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.timeout = 1
+
+# Method to correctly turn OFF and close uRAD
+def closeProgram():
+	# switch OFF uRAD
+	return_code = uRAD_USB_SDK11.turnOFF(ser)
+	if (return_code != 0):
+		exit()
+
+# Open serial port
+try:
+	ser.open()
+except:
+	closeProgram()
+
+# switch ON uRAD
+return_code = uRAD_USB_SDK11.turnON(ser)
+if (return_code != 0):
+	closeProgram()
+
+if (not usb_communication):
+	sleep(timeSleep)
+
+# loadConfiguration uRAD
+return_code = uRAD_USB_SDK11.loadConfiguration(ser, mode, f0, BW, Ns, Ntar, Rmax, MTI, Mth, Alpha, distance_true, velocity_true, SNR_true, I_true, Q_true, movement_true)
+if (return_code != 0):
+	closeProgram()
+
+if (not usb_communication):
+	sleep(timeSleep)
+
+# resultsFileName = 'IQ.txt'
+# fileResults = open(resultsFileName, 'a')
+iterations = 0
+t_0 = time()
+
+# infinite detection loop
+while True:
+
+	# target detection request
+	return_code, results, raw_results = uRAD_USB_SDK11.detection(ser)
+	if (return_code != 0):
+		closeProgram()
+
+	# Extract results from outputs
+	I = raw_results[0]
+	Q = raw_results[1]
+
+	t_i = time()
+
+	# IQ_string = ''
+	# for index in range(len(I)):
+	# 	IQ_string += '%d ' % I[index]
+	# for index in range(len(Q)):
+	# 	IQ_string += '%d ' % Q[index]
+
+	# fileResults.write(IQ_string + '%1.3f\n' % t_i)
+
+	iterations += 1
+
+	if (iterations > 100):
+		print('Fs %1.2f Hz' % (iterations/(t_i-t_0)))
+
+	if (not usb_communication):
+		sleep(timeSleep)
